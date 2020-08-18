2181fa292a383ea0bdd5ffa9c46b023493b078aa
hiru558 <hirunisenevirathna97@gmail.com>
2020-08-18 23:33:31 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/intentsproj/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.intentsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Toast toast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OnClickButtonListener();
+
+        this.setTitle("My Application");
+    }
+
+    public void OnClickButtonListener(){
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Context context = getApplicationContext();
+                        CharSequence text = "Sending Message";
+                        int duration = Toast.LENGTH_SHORT;
+                        toast = Toast.makeText(context , text, duration);
+                        toast.setGravity(Gravity.CENTER_VERTICAL , 0, 0);
+                        toast.show();
+
+                        Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
